81be08cc643b3da7b2eda5ee8b0dd3f51f6cea85
kornpakSittikool <korapatsittkool@gmail.com>
2025-05-31 05:56:44 +0700

    test husky hook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  selector: 'app-root', // ชื่อ component ที่จะเอาไปใช้ใน HTML เช่น <app-root></app-root>
+  // imports: [RouterOutlet], // ตัวนี้สำหรับ standalone component เท่านั้น ใส่พวก directives, components, pipes ที่ template จะใช้
+  templateUrl: './app.component.html', // ไฟล์ HTML ที่เป็น template ของ component นี้
+  styleUrl: './app.component.scss' // ไฟล์ CSS/SCSS ที่จะใช้กับ component นี้ (จริงๆ ต้องเป็น styleUrls เป็น plural นะ)
 })
 export class AppComponent {
 }
