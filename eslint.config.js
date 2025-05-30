736c946fb0ecd320dbf31b6d72ab612270ecb99e
kornpakSittikool <korapatsittkool@gmail.com>
2025-05-31 06:04:53 +0700

    test hook prettierrc

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,19 @@
 module.exports = [
-    {
-        files: ["src/**/*.ts"],
-        languageOptions: {
-            parser: require("@typescript-eslint/parser"),
-            parserOptions: {
-                project: "./tsconfig.json",
-                ecmaVersion: 2020,
-                sourceType: "module"
-            }
-        },
-        plugins: {
-            "@typescript-eslint": require("@typescript-eslint/eslint-plugin")
-        },
-        rules: {
-            "@typescript-eslint/no-unused-vars": "error"
-        }
-    }
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: require('@typescript-eslint/parser'),
+      parserOptions: {
+        project: './tsconfig.json',
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'error',
+    },
+  },
 ];
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-root', // ชื่อ component ที่จะเอาไปใช้ใน HTML เช่น <app-root></app-root>
   // imports: [RouterOutlet], // ตัวนี้สำหรับ standalone component เท่านั้น ใส่พวก directives, components, pipes ที่ template จะใช้
   templateUrl: './app.component.html', // ไฟล์ HTML ที่เป็น template ของ component นี้
-  styleUrl: './app.component.scss' // ไฟล์ CSS/SCSS ที่จะใช้กับ component นี้ (จริงๆ ต้องเป็น styleUrls เป็น plural นะ)
+  styleUrl: './app.component.scss', // ไฟล์ CSS/SCSS ที่จะใช้กับ component นี้ (จริงๆ ต้องเป็น styleUrls เป็น plural นะ)
 })
-export class AppComponent {
-}
+export class AppComponent {}
