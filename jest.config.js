9916a606f1f00779989d5b20a8041a2b91fff116
kornpakSittikool <korapatsittkool@gmail.com>
2025-05-31 06:23:59 +0700

    test uninstall jest

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  testMatch: ['**/*.spec.ts'],
-  transform: {
-    '^.+\\.(ts|html)$': 'ts-jest',
-  },
-  testEnvironment: 'jsdom',
-};
