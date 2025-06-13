b3369675b2ce98fefd841328ed7f49e0ed05c2b4
kornpakSittikool <korapatsittkool@gmail.com>
2025-06-13 15:19:38 +0700

    [UPDATE] carousel mockup

--- src/app/pages/book-trip/components/body-book-trip/body-book-trip.component.ts
+++ src/app/pages/book-trip/components/body-book-trip/body-book-trip.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CarouselImageComponent } from '../../../../shared/components/carousel-image.component/carousel-image.component';
 import { AirportSelectorComponent } from '../airport-selector/airport-selector.component';
 import {
   dataAirportsByProvinceTH,
@@ -9,7 +10,12 @@ import {
 import { AirportSelector } from './models/airport-seletor/airport-selector-th.model';
 @Component({
   selector: 'app-body-book-trip',
-  imports: [CommonModule, FormsModule, AirportSelectorComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    AirportSelectorComponent,
+    CarouselImageComponent,
+  ],
   standalone: true,
   templateUrl: './body-book-trip.component.html',
   styleUrls: ['./body-book-trip.component.scss'],
---
+++ src/app/shared/components/carousel-image.component/carousel-image.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carousel-image',
+  standalone: true,
+  imports: [],
+  templateUrl: './carousel-image.component.html',
+  styleUrls: ['./carousel-image.component.scss'],
+})
+export class CarouselImageComponent {}
