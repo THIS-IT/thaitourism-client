f74d248f0b03410128e0bc2d5717e2436b9045e8
kornpakSittikool <korapatsittkool@gmail.com>
2025-06-13 23:46:25 +0700

    [UPDATE] change file name

--- src/app/pages/book-trip/components/body-book-trip/body-book-trip.component.ts
+++ src/app/pages/book-trip/components/body-book-trip/body-book-trip.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { CarouselImageComponent } from '../../../../shared/components/carousel-image.component/carousel-image.component';
+
+import { CarouselImageComponent } from '../../../../shared/components/carousel-image/carousel-image.component';
 import { AirportSelectorComponent } from '../airport-selector/airport-selector.component';
 import {
   dataAirportsByProvinceTH,
